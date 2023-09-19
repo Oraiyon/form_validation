ad2501f2876008f167681d14140c4c8cd6693cbe
Oliver Trinidad <ocmtrinidad@gmail.com>
2023-09-19 18:45:02 +0800

    validateZipCode() now checks for input length & password.value === confirmPassword.value to submit

--- index.js
+++ index.js
@@ -36,16 +36,10 @@ const validateZipCode = () => {
         zipCodeError.classList.add("activeError");
         zipCodeError.innerText = "Zip Code Required"
         zipCode.setAttribute("style", "border-bottom: 2px solid red")
-    // zipCode length requirement not working
-    } else if (zipCode.validity.tooShort) {
+    } else if (zipCode.validity.rangeUnderflow || zipCode.validity.rangeOverflow) {
         zipCodeError.classList.add("activeError");
         zipCodeError.innerText = "Zip Codes Are 5 Numbers Long"
         zipCode.setAttribute("style", "border-bottom: 2px solid red")
-    } else if (zipCode.validity.tooLong) {
-        zipCodeError.classList.add("activeError");
-        zipCodeError.innerText = "Zip Codes Are 5 Numbers Long"
-        zipCode.setAttribute("style", "border-bottom: 2px solid red")
-    // 
     } else {
         zipCodeError.innerText = ""
         zipCode.setAttribute("style", "border-bottom: 2px solid green")
@@ -65,7 +59,7 @@ const validatePassword = () => {
 const validateConfirmPassword = () => {
     if (confirmPassword.value !== password.value){
         confirmPasswordError.classList.add("activeError")
-        confirmPasswordError.innerText = "Check Password"
+        confirmPasswordError.innerText = "Match Your Password"
         confirmPassword.setAttribute("style", "border-bottom: 2px solid red")
     } else {
         confirmPasswordError.innerText = ""
@@ -97,8 +91,7 @@ const removeAllStyles = () => {
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
-    if (email.validity.valid && country.validity.valid && zipCode.validity.valid && password.validity.valid && confirmPassword.validity.valid) {
-        console.log("Submitted")
+    if (email.validity.valid && country.validity.valid && zipCode.validity.valid && password.validity.valid && confirmPassword.validity.valid && password.value === confirmPassword.value) {
         validateForm()
         removeAllStyles();
         form.reset()
